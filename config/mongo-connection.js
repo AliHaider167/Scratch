68b208a5cd33df0f431aedf21bb85a8d8a516aae
AliHaider167 <alihaider16703@gmail.com>
2025-09-08 23:28:33 +0500

    Setup the development json file and set the environment variables for development mode and prtect the routes for the development mode only and protect the url of the db for database connection and ristrict to create on one owner one time

--- config/mongo-connection.js
+++ config/mongo-connection.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const dbgr = require("debug")("development:mongo-connection");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/grand-project")
+  .connect(`${config.get("MONGODB_URI")}/grand-project`)
   .then(function () {
-    console.log("Connected to MongoDB");
+    dbgr("Connected to MongoDB");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/", function (req, res) {
   res.send("Hello from Owner Routes");
 });
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    const owner = await ownerModel.find();
+    if (owner.length > 0) {
+      res.status(400).send("Owner already exists");
+    }
+
+    const { fullname, email, password, gstin } = req.body;
+
+    bcrypt.genSalt(10, function (err, salt) {
+      if (!err) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+          if (!err) {
+            const createdOwner = await ownerModel.create({
+              fullname,
+              email,
+              password: hash,
+              gstin,
+            });
+            res.send(createdOwner);
+          }
+        });
+      }
+    });
+  });
+}
 
 module.exports = router;
